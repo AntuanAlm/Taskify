b40d9ae55cd72f4821b0c6cca23797c8d1e03551
Antonio Almansa! <antonioalmansa@hotmail.com>
2025-07-31 15:31:32 +0200

    Actualizo código fuente y preparación para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { TaskProviderWrapper } from './context/task.context.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { HashRouter } from 'react-router-dom';
+import { TaskProviderWrapper } from './context/task.context.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Taskify/">
+  <HashRouter>
     <TaskProviderWrapper>
       <App />
     </TaskProviderWrapper>
-  </BrowserRouter>
-)
+  </HashRouter>
+);
