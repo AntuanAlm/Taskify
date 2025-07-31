1d3e1567fe63893f9f2eb7ed2c9525ba527852b6
Antonio Almansa! <antonioalmansa@hotmail.com>
2025-07-31 15:19:38 +0200

    Cambiando el main otra vez

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,14 @@
-// import React from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import { BrowserRouter } from 'react-router-dom'
-// import { TaskProviderWrapper } from './context/task.context.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <BrowserRouter basename="/Taskify/">
-//     <TaskProviderWrapper>
-//       <App />
-//     </TaskProviderWrapper>
-//   </BrowserRouter>
-// )
-
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { TaskProviderWrapper } from './context/task.context.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter basename="/Taskify/">
     <TaskProviderWrapper>
       <App />
     </TaskProviderWrapper>
-  </HashRouter>
+  </BrowserRouter>
 )
-
-
