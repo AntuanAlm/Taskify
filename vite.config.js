f90dd8d340048f452f0dd581a9d138da93b2bbe4
Antonio Almansa! <antonioalmansa@hotmail.com>
2025-07-31 15:14:23 +0200

    Modificando de nuevo

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Taskify/',
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-  },
 })
