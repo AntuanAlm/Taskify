1f819300a0c5a74f4799d0aee0058af640b28629
Antonio Almansa! <antonioalmansa@hotmail.com>
2025-07-31 15:46:55 +0200

    Proyecto funcionando sin subir a GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Taskify/",
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
 })
